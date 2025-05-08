35a0ca631c1a75ed8cf57f866c5faf7958524671
Stefan Münnich <stefan.muennich@unibas.ch>
2025-05-08 17:08:10 +0200

    Update pubs.js

--- pubs.js
+++ pubs.js
@@ -1,16 +1,13 @@
 document.addEventListener('DOMContentLoaded', function () {
     const authors = document.querySelectorAll('.bibbase_paper_author');
     authors.forEach(author => {
-        // Trim any extra whitespace
         let text = author.textContent.trim();
 
-        // Check if the text ends with "editor."
-        if (text.endsWith('editor.')) {
-            // Remove the trailing dot
+        if (text.endsWith(', editor.')) {
+            text = text.slice(0, -8) + ' (Hg.)';
             text = text.slice(0, -1);
         }
 
-        // Update the text and append the colon
         author.textContent = text + ':';
     });
 });
