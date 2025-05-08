b987c8a0614ce7ed63baaadb941fea8ccd716a37
Stefan Münnich <stefan.muennich@unibas.ch>
2025-05-08 17:54:37 +0200

    Update pubs.js

--- pubs.js
+++ pubs.js
@@ -1,13 +1,19 @@
 document.addEventListener('DOMContentLoaded', function () {
     const authors = document.querySelectorAll('.bibbase_paper_author');
     authors.forEach(author => {
-        let text = author.textContent.trim();
-
-        if (text.endsWith(', editor.')) {
-            console.log('Found editor string in ', text)
-            text = text.slice(0, -9) + ' (Hg.)';
-        }
-
-        author.textContent = text + ':';
+        author.textContent = processAuthorText(author.textContent.trim());
     });
 });
+
+function processAuthorText(text) {
+    if (text.endsWith(', editor.')) {
+        console.log('Found editor in text:', text);
+        return text.slice(0, -9) + ' (Hg.):';
+    } else if (text.endsWith(', editor(s).')) {
+        console.log('Found editor(s) in text:', text);
+        return text.slice(0, -12) + ' (Hgg.):';
+    } else {
+        console.log('No editor found in text:', text);
+    }
+    return text + ':';
+}
