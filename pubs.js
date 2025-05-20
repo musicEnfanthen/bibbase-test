2f4a430066d91c1758e0e343eaf83023f20b35fe
Stefan Münnich <stefan.muennich@unibas.ch>
2025-05-20 13:49:47 +0200

    Update pubs.js

--- pubs.js
+++ pubs.js
@@ -10,7 +10,7 @@ document.addEventListener('DOMContentLoaded', function () {
 function processAuthorText(text) {
     if (text.endsWith('editor.')) {
         console.log('Found editor in text:', text);
-        console.log(text.slice(0, -10);
+        console.log(text.slice(0, -10));
         return text.slice(0, -10) + ' (Hg.):';
     } else if (text.endsWith('editor(s).')) {
         console.log('Found editor(s) in text:', text);
